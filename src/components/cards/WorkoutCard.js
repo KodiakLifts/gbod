615890637098fc6100538ca975fe0aab4aa4cc34
Patrick Leishman <kodiaklifts@gmail.com>
2018-09-30 14:25:16 -0600

    Refactored WorkoutCard into class component.

--- src/components/cards/WorkoutCard.js
+++ src/components/cards/WorkoutCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import SetButton from '../../components/buttons/SetButton';
@@ -9,62 +9,74 @@ const TEXTSTYLE = require('../../styles/TextStyle');
 const COLORS = require('../../styles/Colors');
 const CONTAINERSTYLE = require('../../styles/ContainerStyle');
 
-const WorkoutCard = ({ exerciseId, sets, name, borderStyle, supersetNext }) => {
-  const setButtons = createSetButtons(exerciseId, sets);
-  return (
-    <View >
-      <View style={{ alignItems: 'center' }}>
-        <View style={borderStyle}>
-          <View style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-          }}>
-            <TouchableOpacity>
-              <Text style={TEXTSTYLE.listItem}>
-                {name}
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Icon name={'ellipsis-h'} size={25} color={COLORS.SECONDARYCOLOR} style={{ marginRight: 12 }} />
-            </TouchableOpacity>
-          </View>
-          <View style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginHorizontal: 6,
-            'flexWrap': 'wrap'
-          }}>
-            {setButtons}
-          </View>
-        </View>
-        <Icon
-          name={'link'}
-          size={15}
-          color={supersetNext ? COLORS.SECONDARYCOLOR : COLORS.BACKCOLOR}
-          style={{ transform: [{ rotateZ: '135deg' }] }} />
-      </View>
-    </View>
-  );
-};
+class WorkoutCard extends Component {
+  constructor(props) {
+    super(props);
 
-const createSetButtons = (exerciseId, sets) => {
-  const setButtons = sets.map((set, index) => {
+    this.createSetButtons = this.createSetButtons.bind(this);
+  }
+
+
+  createSetButtons(exerciseId, sets) {
+    const setButtons = sets.map((set, index) => {
+      return (
+        <SetButton
+          key={index}
+          exerciseId={exerciseId}
+          setId={set.id}
+          reps={set.reps}
+          weight={set.weight}
+          type={set.type} />
+      );
+    });
     return (
-      <SetButton
-        key={index}
-        exerciseId={exerciseId}
-        setId={set.id}
-        reps={set.reps}
-        weight={set.weight}
-        type={set.type} />
+      setButtons
     );
-  });
-  return (
-    setButtons
-  );
-};
+  }
+
+  render() {
+    const { exerciseId, sets, name, borderStyle, supersetNext } = this.props;
+    const setButtons = this.createSetButtons(exerciseId, sets);
+    return (
+      <View >
+        <View style={{ alignItems: 'center' }}>
+          <View style={borderStyle}>
+            <View style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between'
+            }}>
+              <TouchableOpacity>
+                <Text style={TEXTSTYLE.listItem}>
+                  {name}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Icon name={'ellipsis-h'} size={25} color={COLORS.SECONDARYCOLOR} style={{ marginRight: 12 }} />
+              </TouchableOpacity>
+            </View>
+            <View style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginHorizontal: 6,
+              'flexWrap': 'wrap'
+            }}>
+              {setButtons}
+            </View>
+          </View>
+          <Icon
+            name={'link'}
+            size={15}
+            color={supersetNext ? COLORS.SECONDARYCOLOR : COLORS.BACKCOLOR}
+            style={{ transform: [{ rotateZ: '135deg' }] }} />
+        </View>
+      </View>
+    );
+  }
+}
+
+
 
 WorkoutCard.propTypes = {
   exerciseId: PropTypes.number,
