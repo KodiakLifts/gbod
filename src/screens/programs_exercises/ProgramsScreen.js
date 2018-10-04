efde996aa9749abe710c8c77ff1740ba2d22d4b4
Patrick Leishman <kodiaklifts@gmail.com>
2018-10-03 18:18:41 -0600

    Made programs, exercises, logs, and stats barebones.

--- src/screens/programs_exercises/ProgramsScreen.js
+++ src/screens/programs_exercises/ProgramsScreen.js
@@ -1,32 +1,24 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import SubScreenTemplate from '../templates/SubScreenTemplate';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import ListCard from '../../components/cards/ListCard';
-
-const COLORS = require('../../styles/Colors');
-const TEXTSTYLE = require('../../styles/TextStyle');
+import PropTypes from 'prop-types';
 
 const STYLE = require('./PEStyle');
 
-const menuItems = [{ name: "option 1" }, { name: "option 2" }];
+const Programs = (props) => {
 
-class Programs extends Component {
-  render() {
-    return (
-      <SubScreenTemplate
-        headerContent={
-          <View />
-        }
-        scrollContent={
-          <ListCard headerTitle="A" items={[
-            { name: 'Exercise 1', details: '125x5, 125x5, 125x5+', options: menuItems },
-            { name: 'Exercise 2', details: '125x5, 125x5, 125x5+', options: menuItems }
-          ]} />
-        }
-      />
-    );
-  }
-}
+  return (
+    <SubScreenTemplate
+      headerContent={
+        <View />
+      }
+      scrollContent={<View />}
+    />
+  );
+};
+
+Programs.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.object)
+};
 
 export default Programs;
