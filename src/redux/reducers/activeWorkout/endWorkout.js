ec0e172b5c0586692363a87d227d5dd78d98e655
Patrick Leishman <kodiaklifts@gmail.com>
2018-10-18 21:49:43 -0600

    Finish workout adds a log.

--- src/redux/reducers/activeWorkout/endWorkout.js
+++ src/redux/reducers/activeWorkout/endWorkout.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export const resetWorkout = state => {
   const activeProgram = state.activeWorkout.program;
   const activeDay = state.activeWorkout.day;
@@ -34,16 +36,115 @@ export const resetWorkout = state => {
 
 export const finishWorkout = state => {
   const activeProgram = state.activeWorkout.program;
-  const days = state.programs[activeProgram].days;
+  const currentActiveDay = state.activeWorkout.day;
+  const date = moment(new Date()).format("YYYY-MM-DD");
+  const updatedSets = state.programs[activeProgram].sets;
 
-  let activeDay = state.activeWorkout.day;
-  activeDay === days.length - 1 ? (activeDay = 0) : activeDay++;
-
-  const exercises = state.programs[activeProgram].exercises.filter(exercise => {
-    return exercise.day === activeDay;
+  const setsToLog = updatedSets.filter(set => {
+    return set.day === currentActiveDay && set.complete;
   });
 
-  const currentExercise = exercises[0].id;
+  const daysExercises = state.programs[activeProgram].exercises.filter(
+    exercise => {
+      return exercise.day === currentActiveDay;
+    }
+  );
+
+  const exercisesToLog = [];
+  daysExercises.forEach(exercise => {
+    const setExercise = setsToLog.find(set => {
+      return set.exercise === exercise.id;
+    });
+    if (setExercise !== undefined) {
+      const found = exercisesToLog.find(e => {
+        exercise === e;
+      });
+      if (found === undefined) {
+        exercisesToLog.push(exercise);
+      }
+    }
+  });
+
+  let newExerciseLibrary = [...state.exerciseLibrary];
+  if (exercisesToLog.length !== 0) {
+    exercisesToLog.forEach(exercise => {
+      newExerciseLibrary = state.exerciseLibrary.map(e => {
+        if (e.id === exercise.libraryId) {
+          let workoutsTowardsIncrease = exercise.workoutsTowardsIncrease + 1;
+          if (workoutsTowardsIncrease === exercise.workoutsToIncrease) {
+            workoutsTowardsIncrease = 0;
+            updatedSets.forEach(set => {
+              if (set.exercise === exercise.id) {
+                set.weight += exercise.increaseAmmount;
+              }
+            });
+          }
+          return {
+            ...e,
+            logs: state.exerciseLibrary[exercise.libraryId].logs.concat({
+              id: state.exerciseLibrary[exercise.libraryId].logs.length,
+              date: date,
+              program: activeProgram,
+              day: currentActiveDay,
+              supersetNext: exercise.supersetNext,
+              includeWarmup: exercise.includeWarmup,
+              workoutsToIncrease: exercise.workoutsToIncrease,
+              increaseAmmount: exercise.increaseAmmount,
+              workoutsTowardsIncrease: workoutsTowardsIncrease,
+              barType: exercise.barType,
+              units: exercise.units,
+              sets: setsToLog.map(set => {
+                if (set.exercise === exercise.id) {
+                  return {
+                    reps: set.reps,
+                    weight: set.weight,
+                    type: set.type,
+                    restMinutes: set.restMinutes,
+                    restSeconds: set.restSeconds,
+                    timerOn: set.timerOn,
+                    percentage: set.percentage,
+                    percent: set.percent
+                  };
+                }
+              })
+            })
+          };
+        }
+        return e;
+      });
+    });
+  }
+
+  const newWorkoutLog = {
+    id: state.workoutLogs.length,
+    date: date,
+    program: state.activeWorkout.program,
+    day: state.activeWorkout.day,
+    notes: state.activeWorkout.notes,
+    libraryExercises: exercisesToLog.map(exercise => {
+      return exercise.libraryId;
+    })
+  };
+
+  const days = state.programs[activeProgram].days;
+
+  let nextActiveDay = currentActiveDay;
+  nextActiveDay === days.length - 1 ? (nextActiveDay = 0) : nextActiveDay++;
+
+  const nextExercises = state.programs[activeProgram].exercises.filter(
+    exercise => {
+      return exercise.day === nextActiveDay;
+    }
+  );
+
+  let currentExercise = 0;
+  if (nextExercises.length !== 0) {
+    currentExercise = nextExercises[0].id;
+  }
+
+  updatedSets.forEach(set => {
+    set.complete = false;
+  });
 
   const newState = {
     ...state,
@@ -53,21 +154,22 @@ export const finishWorkout = state => {
     },
     activeWorkout: {
       ...state.activeWorkout,
-      day: activeDay,
+      day: nextActiveDay,
       currentExercise: currentExercise,
-      dayBarActive: false
+      dayBarActive: false,
+      notes: ""
     },
     programs: state.programs.map(program => {
       if (program.id === activeProgram) {
         return {
           ...program,
-          sets: state.programs[activeProgram].sets.map(set => {
-            return { ...set, ...{ complete: false } };
-          })
+          sets: updatedSets
         };
       }
       return program;
-    })
+    }),
+    exerciseLibrary: newExerciseLibrary,
+    workoutLogs: state.workoutLogs.concat(newWorkoutLog)
   };
 
   return newState;
