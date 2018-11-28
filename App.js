35a9f4464887c39f976908b3fea5bfa7dc885122
Patrick Leishman <kodiaklifts@gmail.com>
2018-11-28 15:04:16 -0700

    Fixed dependencies. App compiles again.

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 
 import Main from "./src/Main";
 import { workoutData } from "./src/redux/reducers";
